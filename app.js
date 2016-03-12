f1420093bea28068c8c349b1f52ff545e983dd74
Thomas Frei <mail@thomasfrei.de>
2016-03-12 10:58:05 +0100

    updated app.js for better console output

--- app.js
+++ app.js
@@ -4,13 +4,7 @@
 
 require("./opcua-server.js").newOpcuaServer();
 var opcua = require("./opcua-server.js");
-
-function work() {
-  console.log('working...');
-  opcua.setBusy(false);
-  opcua.setDone(true);
-}
-var work = _.once(work);
+var _ = require("underscore");
 
 // Implement easy state machine
 setInterval(function() {
@@ -25,13 +19,19 @@ setInterval(function() {
   // Incoming Execute === TRUE
   if (state.execute === true && state.ready === true && state.busy === false
       && state.done === false) {
-    opcua.setReady(false);
-    opcua.setDone(true);
+    console.log("EXECUTING......");
+    setTimeout(function(){
+      console.log("....done");	    
+      opcua.setReady(false);
+      opcua.setDone(true);
+      console.log("waiting for execute to taken back...");
+    },1000);
   }
 
   // Go back to idle state after execution
   if (state.done === true && state.execute === false) {
+    console.log("...ready");
     opcua.setDone(false);
     opcua.setReady(true);
   }
-}, 500);
+}, 1000);
